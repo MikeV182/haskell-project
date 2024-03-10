8468b1af0406cce80c97d9546087f1e4f767b3f9
MikeV182 <145787130+MikeV182@users.noreply.github.com>
2024-03-10 14:07:12 +0700

    added collision

--- pong/app/Main.hs
+++ pong/app/Main.hs
@@ -1,11 +1,16 @@
 module Main(main) where
 
 import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
+
 
 width, height, offset :: Int
 width = 300
 height = 300
 offset = 100
+
+ballRadius :: Float
+ballRadius = 10
 
 window :: Display
 window = InWindow "Pong" (width, height) (offset, offset)
@@ -25,9 +30,9 @@ data PongGame = Game
 -- | The starting state for the game of Pong.
 initialState :: PongGame
 initialState = Game
-  { ballLoc = (-10, 30),
-    ballVel = (1, -3),
-    player1 = 40,
+  { ballLoc = (0, 0),
+    ballVel = (30, -30),
+    player1 = 0,
     player2 = -80
   }
 
@@ -40,13 +45,13 @@ render :: PongGame  -- ^ The game state to render.
 render game = pictures [
     ball,
     walls,
-    makeRectangle blue 120 $ player1 game,
-    makeRectangle orange (-120) $ player2 game
+    makeRectangle blue 120 $ player2 game,
+    makeRectangle orange (-120) $ player1 game
     ]
     where
         -- ball drawing
         -- uncurry translate (1,2) Picture <==> translate 1 2 Picture
-        ball = uncurry translate (ballLoc game) $ color ballColor $ circleSolid 8
+        ball = uncurry translate (ballLoc game) $ color ballColor $ circleSolid ballRadius
         ballColor = dark red
 
         -- walls in window
@@ -76,9 +81,69 @@ moveBall sec game = game {ballLoc = (x', y')}
         x' = x + velX * sec
         y' = y + velY * sec
 
+fps :: Int
+fps = 60
+
+-- | Update the game by moving the ball.
+-- Ignore the ViewPort argument.
+update :: ViewPort -> Float -> PongGame -> PongGame
+update _ sec = paddleBounce . wallBounce . moveBall sec
+
 -- animate :: Display -> Color -> (Float -> Picture) -> IO ()
+-- simulate --> https://hackage.haskell.org/package/gloss-1.13.2.2/docs/Graphics-Gloss-Interface-Pure-Simulate.html#v:simulate
 main :: IO ()
-main = animate window background frame
+main = simulate window background fps initialState render update
+
+type Radius = Float
+type PosOfBall = (Float, Float)
+
+collisionWalls :: PosOfBall -> Radius -> Bool
+collisionWalls (_,y) radius = topWall || bottomWall
+  where -- using fromIntegral to convert numbers from Int to Float type
+    topWall = y - radius <= -fromIntegral height / 2
+    bottomWall = y + radius >= fromIntegral height / 2
+
+wallBounce :: PongGame -> PongGame
+wallBounce game = game { ballVel = (vx, vy') }
+  where
+    -- Radius. Use the same thing as in `render`.
+    radius = ballRadius
+
+    -- The old velocities.
+    (vx, vy) = ballVel game
+
+    vy' = if collisionWalls (ballLoc game) radius
+          then
+             -- Update the velocity.
+             -vy
+           else
+            -- Do nothing. Return the old velocity.
+            vy
+
+
+paddleBounce :: PongGame -> PongGame
+paddleBounce game = game {ballVel = vel}
     where
-        frame :: Float -> Picture
-        frame sec = render $ moveBall sec initialState
+        (vx, vy) = ballVel game
+        (x, y)   = ballLoc game
+        (hitOrNot, paddleY) = paddleCollision game
+        vel = if hitOrNot
+            then (-vx, archvy)
+            else (vx, vy)
+        archvy = abs vx * tan (normalAngle + archedAngle)
+        normalAngle = atan (vy / abs vx)
+        archedAngle = (y - paddleY) / (85 * 0.7)
+
+paddleCollision :: PongGame -> (Bool, Float)
+paddleCollision game = (leftCollision || rightCollision, hit)
+    where
+        (ballX, ballY) = ballLoc game
+        paddleY1 = player1 game
+        paddleY2 = player2 game
+        leftCollision =   ballX < 300 / (-2) + 1.5 * 25 + ballRadius
+                          && ballY < paddleY1 + 85 / 2
+                          && ballY > paddleY1 - 85 / 2
+        rightCollision =  ballX > 300 / 2 - 1.5 * 25 - ballRadius
+                          && ballY < paddleY2 + 85 / 2
+                          && ballY > paddleY2 - 85 / 2
+        hit = if leftCollision then paddleY1 else paddleY2
